ddc3e55f03933b11a915c46a5089fdc74c7b4133
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-03-19 11:01:36 +0530

    Learned Routing parameters and constraint routing parameter

--- laravel-app1/routes/web.php
+++ laravel-app1/routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+//    echo "Hello World!!!", "<br>";
+//    echo "Hello Everyone!!!";
 });
 
 Route::get('/my', function () {
@@ -14,3 +16,38 @@ Route::get('/my', function () {
 Route::get('custom/', [MyController::class, 'index']);
 
 Route::get('/customshow', [MyController::class, 'show']);
+
+
+
+// Routing Parameters :: Laravel Route Parameters
+
+Route::get('/param/{id}', function ($id) {
+    return "<h1> User id is $id";
+});
+
+Route::get('/param/{id1}/{id2}', function ($id1, $id2) {
+    return "<h1> User id is $id1 and $id2</h1>";
+});
+
+// Optional Parameter
+Route::get('/param/{id?}', function ($id = 123) {
+    return "<h1> User id is $id </h1>";
+});
+
+// Routing Constraint :: Accept only a certain type of routing parameter and reject other types with 404 Error
+Route::get('/paramnum/{id}', function ($id) {
+    return "<h1> User id is $id";
+})->whereNumber('id');
+
+Route::get('/paramalpha/{id}', function ($id) {
+    return "<h1> User id is $id";
+})->whereAlpha('id');
+
+Route::get('/paramalphanum/{id}', function ($id) {
+    return "<h1> User id is $id";
+})->whereAlphaNumeric('id');
+
+Route::get('/paramregex/{id}', function ($id) {
+    return "<h1> User id is $id";
+})->where('id', '[0-9]+');
+
