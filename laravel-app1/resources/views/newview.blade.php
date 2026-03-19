cd3d27347e70d058dff89bd6f6605b47f9658e9c
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-03-19 11:34:12 +0530

    Learned Passing data to views

---
+++ laravel-app1/resources/views/newview.blade.php
@@ -0,0 +1,5 @@
+<div>
+    I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison
+
+    <h1>The name is {{ $name }}</h1>
+</div>
--- laravel-app1/routes/web.php
+++ laravel-app1/routes/web.php
@@ -51,3 +51,48 @@ Route::get('/paramregex/{id}', function ($id) {
     return "<h1> User id is $id";
 })->where('id', '[0-9]+');
 
+// Passing multiple parameters
+
+// Example Sum of two numbers
+Route::get('/sum/{num1}/{num2}', function ($num1, $num2) {
+    $sum = $num1 + $num2;
+    return "<h1> The Sum is $sum </h1>";
+})->whereNumber('num1')->whereNumber('num2');
+
+// Example Multiplication Table
+Route::get('/table/{num}', function ($num) {
+    for($i=1;$i<11;$i++) {
+        $result = $num * $i;
+        echo $num . " x " . $i . " = " . $result . "</br>";
+    }
+})->whereNumber('num1');
+
+/*
+    Passing data to the views
+
+    1ST METHOD :: Arrays -> Associative Arrays as a ['key': 'value'] pair
+    2ND METHOD :: with('var_name', 'value') -> as a var_name and it's value pair
+    3RD METHOD :: compact($var1, $var2, $var3) -> multiple parameter
+    4TH METHOD :: withName() -> like withAddress($address) and use camel case to define this methods name.
+*/
+
+// Using Associative array
+Route::get('/newview1', function () {
+    return view('newview', ['name'=>'Raj']);
+});
+
+// Using with()
+Route::get('/newview2', function () {
+    return view('newview')->with('name', "Raj Ranjan");
+});
+
+// Using Compact with static data
+Route::get('/newview3', function () {
+    $name = "Raj Ranjan";
+    return view('newview', compact('name'));
+});
+
+// Using Compact with dynamic data
+Route::get('/newview4/{name}', function ($name) {
+    return view('newview', compact('name'));
+});
